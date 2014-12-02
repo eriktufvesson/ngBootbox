c848cec585e5dd3631636cc67d72fbc6843440c5
eriktufvesson <erik.tufvesson@gmail.com>
2014-12-02 12:05:08 +0100

    Updated examples

--- app/app.js
+++ app/app.js
@@ -2,64 +2,80 @@
  * Created by Erik on 2014-05-30.
  */
 angular.module('testApp', ['ngBootbox'])
-    .controller('TestCtrl', function($scope, $log, $ngBootbox) {
+  .controller('TestCtrl', function($scope, $log, $ngBootbox) {
 
-        $scope.actions = [];
+      $scope.actions = [];
 
-        $scope.addAction = function(type, msg) {
-            console.log(type + ': ' + msg);
-            $scope.actions.push({
-                msg: type + ': ' + msg
+      $scope.addAction = function(type, msg) {
+          console.log(type + ': ' + msg);
+          $scope.actions.push({
+              msg: type + ': ' + msg
+          });
+          //$scope.$apply();
+      };
+
+
+      $scope.handleAlert = function() {
+          $ngBootbox.alert('test1!')
+            .then(function() {
+                $log.log('Alert closed');
             });
-            $scope.$apply();
-        };
+      };
 
+      $scope.handleConfirm = function() {
+          $ngBootbox.confirm('test2!')
+            .then(function() {
+                $log.info('Confirmed!');
+            }, function() {
+                $log.log('Confirm dismissed!');
+            });
+      };
 
-        $scope.handleAlert = function() {
-            $ngBootbox.alert('test1!')
-                .then(function() {
-                    $log.log('Alert closed');
-                });
-        };
+      $scope.handlePrompt = function() {
+          $ngBootbox.prompt('test3!')
+            .then(function(result) {
+                $log.info('Prompt returned: ' + result);
+            }, function() {
+                $log.log('Prompt dismissed!');
+            });
+      };
 
-        $scope.handleConfirm = function() {
-            $ngBootbox.confirm('test2!')
-                .then(function() {
-                    $log.info('Confirmed!');
-                }, function() {
-                    $log.log('Confirm dismissed!');
-                });
-        };
+      $scope.customDialogButtons = {
+          warning: {
+              label: "Warning!",
+              className: "btn-warning",
+              callback: function() { $scope.addAction('Warning', false); }
+          },
+          success: {
+              label: "Success!",
+              className: "btn-success",
+              callback: function() { $scope.addAction('Success!', true) }
+          },
+          danger: {
+              label: "Danger!",
+              className: "btn-danger",
+              callback: function() { $scope.addAction('Danger!', false) }
+          },
+          main: {
+              label: "Click ME!",
+              className: "btn-primary",
+              callback: function() { $scope.addAction('Main...!', true) }
+          }
+      };
 
-        $scope.handlePrompt = function() {
-            $ngBootbox.prompt('test3!')
-                .then(function(result) {
-                    $log.info('Prompt returned: ' + result);
-                }, function() {
-                    $log.log('Prompt dismissed!');
-                });
-        };
+      $scope.customConfirmButtons = {
+          ok: {
+              label: "Ok",
+              className: "btn-primary",
+              callback: function() { $scope.deleteBook(); }
+          },
+          cancel: {
+              label: "Cancel",
+              className: "btn-default"
+          }
+      };
 
-        $scope.customDialogButtons = {
-            warning: {
-                label: "Warning!",
-                className: "btn-warning",
-                callback: function() { $scope.addAction('Warning', false); }
-            },
-            success: {
-                label: "Success!",
-                className: "btn-success",
-                callback: function() { $scope.addAction('Success!', true) }
-            },
-            danger: {
-                label: "Danger!",
-                className: "btn-danger",
-                callback: function() { $scope.addAction('Danger!', false) }
-            },
-            main: {
-                label: "Click ME!",
-                className: "btn-primary",
-                callback: function() { $scope.addAction('Main...!', true) }
-            }
-        };
-    });
+      $scope.deleteBook = function() {
+          $ngBootbox.alert('Book deleted!');
+      };
+  });
